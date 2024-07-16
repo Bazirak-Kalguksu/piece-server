036d7a8f7890fdb323da602546012d62ba1b7136
lgwk42 <luisgunkim@gmail.com>
2024-07-16 20:10:10 +0900

    :bug: :: jpa 오류 수정

--- src/main/java/com/project/pieceserver/domain/user/domain/repository/jpa/UserRepository.java
+++ src/main/java/com/project/pieceserver/domain/user/domain/repository/jpa/UserRepository.java
@@ -3,5 +3,5 @@ package com.project.pieceserver.domain.user.domain.repository.jpa;
 import com.project.pieceserver.domain.user.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }

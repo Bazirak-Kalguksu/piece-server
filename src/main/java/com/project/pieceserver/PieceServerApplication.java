b7b821ddccf9dc7d59a9ba037ccbcf23c06cbf53
lgwk42 <luisgunkim@gmail.com>
2024-07-16 19:43:30 +0900

    :sparkles: :: 서버 세팅

---
+++ src/main/java/com/project/pieceserver/PieceServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.pieceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PieceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/pieceserver/PieceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pieceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

b667e4b8773d2cbf6b7a8ec0f29ca5996eb883be
lgwk42 <luisgunkim@gmail.com>
2024-07-16 20:14:05 +0900

    :sparkles: :: user enums 추가

--- src/main/java/com/project/pieceserver/domain/user/domain/enums/UserRole.java
+++ src/main/java/com/project/pieceserver/domain/user/domain/enums/UserRole.java
@@ -1,4 +1,14 @@
 package com.project.pieceserver.domain.user.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
+
+    USER("ROLE_USER");
+
+    private final String key;
+
 }

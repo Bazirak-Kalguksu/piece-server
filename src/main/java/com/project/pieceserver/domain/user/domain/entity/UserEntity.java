4c7961fd87ca784a271163076ea3425cb30aa820
lgwk42 <luisgunkim@gmail.com>
2024-07-16 20:09:35 +0900

    :sparkles: :: 유저 테이블 컬럼 추가

--- src/main/java/com/project/pieceserver/domain/user/domain/entity/UserEntity.java
+++ src/main/java/com/project/pieceserver/domain/user/domain/entity/UserEntity.java
@@ -1,4 +1,30 @@
 package com.project.pieceserver.domain.user.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@Table(name = "tb_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity {
+
+    @Id
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String birth;
+
 }

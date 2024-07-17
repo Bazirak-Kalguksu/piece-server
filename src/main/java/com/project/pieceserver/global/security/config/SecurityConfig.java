d33178acaf5dbd6b6fc18c06f85119ecfd3039d8
lgwk42 <luisgunkim@gmail.com>
2024-07-17 12:12:41 +0900

    :sparkles: :: 보안 설정 추가

---
+++ src/main/java/com/project/pieceserver/global/security/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.project.pieceserver.global.security.config;
+
+import com.project.pieceserver.global.security.jwt.filter.JwtAuthenticationFilter;
+import com.project.pieceserver.global.security.jwt.filter.JwtExceptionFilter;
+import com.project.pieceserver.global.security.jwt.handler.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtExceptionFilter jwtExceptionFilter;
+
+    private static final String USER = "ROLE_USER";
+    private static final String TEACHER = "ROLE_TEACHER";
+    private static final String ADMIN = "ROLE_ADMIN";
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .exceptionHandling(handlingConfigures -> handlingConfigures.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+        return http.build();
+    }
+
+}

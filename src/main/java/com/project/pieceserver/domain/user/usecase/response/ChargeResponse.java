807d49bd0a7bc252a10e71737027a9ec5eef558c
lgwk42 <luisgunkim@gmail.com>
2024-07-18 04:22:34 +0900

    :sparkles: 정

--- src/main/java/com/project/pieceserver/domain/user/usecase/response/ChargeResponse.java
+++ src/main/java/com/project/pieceserver/domain/user/usecase/response/ChargeResponse.java
@@ -1,8 +1,16 @@
 package com.project.pieceserver.domain.user.usecase.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChargeResponse {
 
     private int currentBalance;

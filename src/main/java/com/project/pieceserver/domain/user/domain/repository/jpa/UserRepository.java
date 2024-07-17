e5d640f0f847536b5c0aed3864b48375ec9ad475
su080915 <164843727+su080915@users.noreply.github.com>
2024-07-17 10:30:47 +0900

    Revert "feat/user"

--- src/main/java/com/project/pieceserver/domain/user/domain/repository/jpa/UserRepository.java
+++ src/main/java/com/project/pieceserver/domain/user/domain/repository/jpa/UserRepository.java
@@ -1,15 +1,7 @@
 package com.project.pieceserver.domain.user.domain.repository.jpa;
 
 import com.project.pieceserver.domain.user.domain.entity.UserEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<UserEntity, String> {
-
-    Optional<UserEntity> findByEmail(String email);
-
-    UserEntity getByEmail(String email);
-
 }

83c70ea2f88a6d8d4f640d39821484a7e845ee25
lgwk42 <luisgunkim@gmail.com>
2024-07-17 12:11:53 +0900

    :sparkles: :: 설정 클래스 추가

---
+++ src/main/java/com/project/pieceserver/global/common/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.project.pieceserver.global.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** swagger 설정 파일*/
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Piece")
+                .description("Piece API Document")
+                .version("0.0.1");
+    }
+
+}

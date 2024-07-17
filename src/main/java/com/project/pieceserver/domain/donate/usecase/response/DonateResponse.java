d2308a00cadc5a78878315a6336c336b0828cc81
lgwk42 <luisgunkim@gmail.com>
2024-07-18 04:23:52 +0900

    :sparkles: :: 후원 리스폰스 수정

--- src/main/java/com/project/pieceserver/domain/donate/usecase/response/DonateResponse.java
+++ src/main/java/com/project/pieceserver/domain/donate/usecase/response/DonateResponse.java
@@ -1,8 +1,16 @@
 package com.project.pieceserver.domain.donate.usecase.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DonateResponse{
     private int balance;
 }

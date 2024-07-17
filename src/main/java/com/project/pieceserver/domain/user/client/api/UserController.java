3ca009eceaf011f4d9b50804befec76058596279
lgwk42 <luisgunkim@gmail.com>
2024-07-17 13:12:15 +0900

    :sparkles: :: swagger 어너테이션 추가

--- src/main/java/com/project/pieceserver/domain/user/client/api/UserController.java
+++ src/main/java/com/project/pieceserver/domain/user/client/api/UserController.java
@@ -2,6 +2,8 @@ package com.project.pieceserver.domain.user.client.api;
 
 import com.project.pieceserver.domain.user.application.service.UserService;
 import com.project.pieceserver.domain.user.client.dto.request.NameEditRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "유저 API")
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
-
-    @PatchMapping("/user")
+    @PatchMapping("")
+    @Operation(summary = "유저 이름 변경")
     public void editUser(@Validated @RequestBody NameEditRequest request) {
         userService.editUserName(request);
     }
+
 }

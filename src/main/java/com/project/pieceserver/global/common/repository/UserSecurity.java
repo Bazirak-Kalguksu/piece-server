9b42d2ed754587627fe32e7503fc96f4501ce934
lgwk42 <luisgunkim@gmail.com>
2024-07-17 12:11:33 +0900

    :sparkles: :: 유저 시큐리티 추가

---
+++ src/main/java/com/project/pieceserver/global/common/repository/UserSecurity.java
@@ -0,0 +1,9 @@
+package com.project.pieceserver.global.common.repository;
+
+import com.project.pieceserver.domain.user.client.dto.User;
+
+public interface UserSecurity {
+
+    User getUser();
+
+}
---
+++ src/main/java/com/project/pieceserver/global/common/repository/UserSecurityImpl.java
@@ -0,0 +1,20 @@
+package com.project.pieceserver.global.common.repository;
+
+import com.project.pieceserver.domain.user.client.dto.User;
+import com.project.pieceserver.global.security.auth.CustomUserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserSecurityImpl implements UserSecurity{
+
+    @Override
+    public User getUser() {
+        return ((CustomUserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal())
+                .getUser();
+    }
+
+}
